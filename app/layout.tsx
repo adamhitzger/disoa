54af8182620b419a229dd762726b55ff1bcbd7ab
adamhitzger <adam.hitzger@icloud.com>
2025-06-21 17:21:04 +0200

    Uprava designu

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono, Sora } from "next/font/google";
+import { Sora } from "next/font/google";
 import "./globals.css";
 
 const sora = Sora({
