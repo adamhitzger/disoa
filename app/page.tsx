24c65033c97f623bab6cfd32827c7ba6d82de2d4
adamhitzger <adam.hitzger@icloud.com>
2025-06-21 17:35:13 +0200

    Uprava designu

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
     <main className="font-sora w-full bg-white flex flex-col  items-center text-black min-h-screen p-8">
       <div className="flex md:w-6/7 flex-col w-full space-y-4 text-center m-auto">
 <h1 className="text-4xl font-medium">Web v rekonstrukci - tvoříme něco lepšího.</h1>
-      <p className="text-gray-500 text-[17px] leading-7">
+      <p className="text-gray-500 text-[17px] leading-8">
         Právě pro vás stavíme nový digitalní prostor, který bude stejně funkční a kreativní jako naše práce pro klienty.<br/>
         Jsme Disoa Marketing - marketingová agentura jež pomáhá značkám růst pomocí strategie, designu a výkonu.<br/>
         Mezitím nás můžete kontaktovat na emailu: <Link className="text-blue-800 underline underline-offset-2 decoration-1 decoration-wavy" href={"mailto:info@disoa.cz"}>info@disoa.cz</Link> nebo sledovat naší práci na sociálních sítích.<br/>
