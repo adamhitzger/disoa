b92875c21f6bb9731fe1801ee362cc977da9f0b1
adamhitzger <adam.hitzger@icloud.com>
2025-06-24 20:06:04 +0200

    Zmena main textu

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
       <div className="flex md:w-6/7 flex-col w-full space-y-4 text-center m-auto">
 <h1 className="text-4xl font-medium">Web v rekonstrukci - tvoříme něco lepšího.</h1>
       <p className="text-gray-500 text-[17px] leading-8">
-        Právě pro vás stavíme nový digitalní prostor, který bude stejně funkční a kreativní jako naše práce pro klienty.<br/>
-        Jsme Disoa Marketing - marketingová agentura jež pomáhá značkám růst pomocí strategie, designu a výkonu.<br/>
-        Mezitím nás můžete kontaktovat na emailu: <Link className="text-blue-800 " href={"mailto:info@disoa.cz"}>info@disoa.cz</Link> nebo sledovat naší práci na sociálních sítích.<br/>
-        Děkujeme za trpělivost. Brzy jsme zpět silnější než kdy dřív.
+       Právě pro vás připravujeme nový digitální prostor, který bude stejně funkční a kreativní jako naše práce pro klienty.<br/>
+       Jsme DISOA - marketingová agentura, která firmám pomáhá růst díky strategii, obsahu a výkonu.<br/>
+        Mezitím nás můžete kontaktovat na e-mailu <Link className="text-blue-800 " href={"mailto:info@disoa.cz"}>info@disoa.cz</Link> nebo sledovat naší práci na sociálních sítích.<br/>
+       Děkujeme za trpělivost. Brzy jsme zpět, silnější než kdy dřív.
       </p>
       <div className="w-fit mx-auto flex gap-5 flex-row justify-between">
         <Link href={"mailto:info@disoa.cz"} className="w-fit flex flex-row gap-3">
